56411b09b01f2121229b8c44eae180cfa24384f6
jslee <jslee@antock.com>
2026-09-18 18:11:47 +0900

    feat: add TodoStatusChip component

---
+++ components/todo/TodoStatusChip.tsx
@@ -0,0 +1,29 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import type { ComponentPropsWithRef } from 'react';
+import { cn } from '@/lib/utils';
+
+// api로 받아오는 done:boolean 값 false:'TO DO' 또는 true:'DONE'에 따라 상태가 바뀝니다.
+const chipVariants = cva(
+  'flex h-[1.4rem] w-12 flex-row flex-nowrap items-center justify-center gap-2 rounded-md px-0.75 py-0.5',
+  {
+    variants: {
+      isTodo: {
+        false: 'bg-[#FEEFDC] text-[#EF6C00]',
+        true: 'bg-[#BBBBBB] text-white',
+      },
+    },
+
+    defaultVariants: { isTodo: false },
+  },
+);
+
+export type TODOChipProps = ComponentPropsWithRef<'span'> &
+  VariantProps<typeof chipVariants>;
+
+export default function TodoStatusChip({ isTodo, className }: TODOChipProps) {
+  return (
+    <div className={cn(chipVariants({ isTodo }), className)}>
+      <span className={'text-xs/4'}>{isTodo ? 'DONE' : 'TO DO'}</span>
+    </div>
+  );
+}
--- types/note.ts
+++ types/note.ts
@@ -1,7 +1,41 @@
-export type NoteStatus = 'TO DO' | 'DONE';
-export interface NoteProps {
+export type TodoStatus = 'TO DO' | 'DONE';
+
+export interface NoteText {
+  type: 'text';
+  text: string;
+}
+
+export interface NoteParagraph {
+  type: 'paragraph';
+  content: NoteText[];
+}
+
+export interface NoteContent {
+  type: 'doc';
+  content: NoteParagraph[];
+}
+
+export interface NoteTodo {
+  id: number;
   title: string;
-  status: NoteStatus | string;
-  content: string;
-  date: string;
+  done: boolean;
+}
+
+export interface Note {
+  id: number;
+  teamId: string;
+  userId: number;
+  todoId: number;
+  title: string;
+  content: NoteContent;
+  linkUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  todo: NoteTodo;
+}
+
+export interface NoteProps {
+  notes: Note[];
+  nextCursor: number | null;
+  totalCount: number;
 }
